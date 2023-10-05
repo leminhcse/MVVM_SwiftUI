86686bbc52c3cc78dcd3e3923afe52d2ba79fd4e
ntphuongHD <phuong.nguyen@hdwebsoft.dev>
2023-10-05 11:08:29 +0700

    Initial Commit

---
+++ MVVM_SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM_SwiftUI
+//
+//  Created by mac on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
